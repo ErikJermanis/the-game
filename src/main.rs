f5635e7b2827c2f3209d8dc726a447ce84de49d3
ErikJermanis <erik55jermanis@gmail.com>
2024-07-26 22:23:19 +0200

    rendering a window, idk what I am doing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
-use winit::event_loop::EventLoop;
-use winit::event_loop::ActiveEventLoop;
-use winit::window::Window;
+use winit::{
+    dpi::LogicalSize, event::*, event_loop::EventLoop, keyboard::{KeyCode, PhysicalKey}, window::WindowBuilder
+};
 
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
+const WIDTH: u16 = 800;
+const HEIGHT: u16 = 600;
 
 fn main() {
+    env_logger::init();
     let event_loop = EventLoop::new().unwrap();
-    let window_attributes = Window::default_attributes().with_title("The Game");
-    ActiveEventLoop::create_window(&self, window_attributes);
+    let size = LogicalSize::new(WIDTH, HEIGHT);
+    let window = WindowBuilder::new()
+        .with_title("The Game")
+        .with_inner_size(size)
+        .with_min_inner_size(size)
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => control_flow.exit(),
+            _ => {}
+        },
+        _ => {}
+    }).unwrap();
 }
