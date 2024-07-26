52f1c2f563cd38263bf1e44d8c0c564c81705fcb
ErikJermanis <erik55jermanis@gmail.com>
2024-07-26 13:09:01 +0200

    skill issues

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use winit::event_loop::EventLoop;
+use winit::event_loop::ActiveEventLoop;
+use winit::window::Window;
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    let window_attributes = Window::default_attributes().with_title("The Game");
+    ActiveEventLoop::create_window(&self, window_attributes);
 }
